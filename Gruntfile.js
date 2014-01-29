ed5c84c6c201ad160716fa9ac063e352a2dc7efa
James Wilson <jwilson1@fossil.com>
2014-01-29 16:59:41 -0600

    fixed gruntfile indenting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,40 @@
 module.exports = function(grunt) {
 
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
-	grunt.initConfig({
-		clean: ['dist'],
-		less: {
-			options: {},
-			target: {
-				files: [
-					{
-						src: ['bower_components/bootstrap/less/bootstrap.less'],
-						dest: 'dist/.tmp/css/bootstrap.css'
-					},
-					{
-						src: ['src/less/**/*.less'],
-						dest: 'dist/.tmp/css'
-					}
-				]
-			}
-		},
-		cssmin: {
-			combine: {
-				files: {
-					'dist/.tmp/css/main.css': ['dist/.tmp/css/**/*.css']
-				}
-			},
-			minify: {
-				src: ['dist/.tmp/css/main.css'],
-				dest: 'dist/public/main.min.css'
-			}
-		}
+  grunt.initConfig({
+    clean: ['dist'],
+    less: {
+      options: {},
+      target: {
+        files: [
+          {
+            src: ['bower_components/bootstrap/less/bootstrap.less'],
+            dest: 'dist/.tmp/css/bootstrap.css'
+          },
+          {
+            src: ['src/less/**/*.less'],
+            dest: 'dist/.tmp/css'
+          }
+        ]
+      }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'dist/.tmp/css/main.css': ['dist/.tmp/css/**/*.css']
+        }
+      },
+      minify: {
+        src: ['dist/.tmp/css/main.css'],
+        dest: 'dist/public/main.min.css'
+      }
+    }
 
-	});
+  });
 
-	grunt.registerTask('build', ['clean', 'less', 'cssmin']);
+  grunt.registerTask('build', ['clean', 'less', 'cssmin']);
 };
